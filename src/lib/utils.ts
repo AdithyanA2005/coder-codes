2724682525e5fd8002e52b64e0e7095ba0c93566
Adithyan A <adithyanthedev@gmail.com>
2025-12-26 15:37:55 +0530

    feat: update metadata to use structured icon definitions instead of a single icon path.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,14 +4,12 @@ export function constructMetadata({
   title = "CoderCodes - KTU CS Lab Programs",
   description = "A comprehensive collection of KTU BTech CS lab programs, simplified for clarity and performance.",
   image = "/opengraph-image.jpg",
-  icons = "/icon.png",
   noIndex = false,
   type = "website",
 }: {
   title?: string;
   description?: string;
   image?: string;
-  icons?: string;
   noIndex?: boolean;
   type?: "website" | "article";
 } = {}): Metadata {
@@ -35,7 +33,11 @@ export function constructMetadata({
       images: [image],
       creator: "@codercodes",
     },
-    icons,
+    icons: {
+      icon: "/icon.png",
+      shortcut: "/favicon.ico",
+      apple: "/apple-icon.png",
+    },
     metadataBase: new URL("https://codercodes.vercel.app"),
     ...(noIndex && {
       robots: {
